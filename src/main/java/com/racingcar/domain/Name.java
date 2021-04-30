aeebb10c9b703903c5833af7ecf0cd4938d6c35c
Bryce Yang <nash31@korea.ac.kr>
2021-04-30 23:03:48 +0900

    feature:자동차 이름 설정
    - 자동차 이름 1~5자리만 가능하도록 구현

---
+++ src/main/java/com/racingcar/domain/Name.java
@@ -0,0 +1,10 @@
+package com.racingcar.domain;
+
+public class Name {
+	private String name;
+
+	public void setName(String name) {
+		if (name.length() == 0 || name.length() > 5) throw new IllegalArgumentException("자동차 이름은 한 글자 이상 다섯 글자 이하만 가능합니다.");
+		this.name = name;
+	}
+}
